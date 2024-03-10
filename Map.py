fc84c33d3fc92afb4e258f6c3b78160f58475b60
Khoa-It <nguyendangyukhoa01@gmail.com>
2024-03-10 14:53:46 +0700

    refining map: add tree and building

--- Map.py
+++ Map.py
@@ -11,8 +11,8 @@ class Map:
     creativeCube = 5
 
     def __init__(self):
-        Entity(model='cube', position=(0,0,0), collider='box', scale=(2500,1,2500), texture='brick', color = color.gray)
-        self.creativeCube = createMyCube(0,0,20,color.red)
+        Entity(model='cube', position=(0,0,0), collider='box', scale=(3500,1,3500), texture='brick', color = color.gray)
+        self.creativeCube = createMyCube(-800,0,20,color.red)
         for pos in listCubePosition:
             createMyCube(pos['x'], pos['z'], pos['height'], pos['color'])
         for pos in listWallPosition:
--- main.py
+++ main.py
@@ -17,6 +17,21 @@ def input(key):
         print("cube:","{",f"'x':{map.creativeCube.x}, 'height':20, 'z':{map.creativeCube.z}, 'color':color.green","},")
         print("wall:","{",f"'x':{map.creativeCube.x}, 'height':80, 'width':150, 'z':{map.creativeCube.z}, 'color':color.rgb(128, 49, 4), 'corner':0 ", "},")
         print("tree:", "{",f"'x':{map.creativeCube.x}, 'z':{map.creativeCube.z}", "},")
+        print("house:", "{", f"'x':{map.creativeCube.x}, 'z':{map.creativeCube.z}, 'corner':0", "},")
+        print("building:",
+              "{",
+              f"'x':{map.creativeCube.x-60}, 'height':80*2, 'width':150*2, 'z':{map.creativeCube.z+200}, 'color':color.rgb(145, 117, 6), 'corner':0 ",
+              "},",
+              "{",
+              f"'x':{map.creativeCube.x+200},'height':80*2, 'width':150*3, 'z':{map.creativeCube.z-60}, 'color':color.rgb(145, 117, 6), 'corner':90 ",
+              "},",
+              "{",
+              f"'x':{map.creativeCube.x}, 'height':80*2, 'width':150*2+100, 'z':{map.creativeCube.z-260}, 'color':color.rgb(145, 117, 6), 'corner':0 ",
+              "},",
+              "{",
+              f"'x':{map.creativeCube.x-200}, 'height':80*2, 'width':150*2, 'z':{map.creativeCube.z+50}, 'color':color.rgb(145, 117, 6), 'corner':90 ",
+              "},",
+              )
     
 
 
