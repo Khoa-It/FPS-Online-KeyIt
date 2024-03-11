a0a85f87b602fdfbd0b21027c739f5601e35af3d
Khoa-It <nguyendangyukhoa01@gmail.com>
2024-03-11 21:33:41 +0700

    add gun and first person controller

--- CustomLib.py
+++ CustomLib.py
@@ -2,12 +2,12 @@ from ursina import *
 
 
 def printInfo():
-    print("Mouse position:",mouse.position)
+    print("Mouse position:", mouse.position)
 
 
 def createMyCube(x, z, width, mycolor) -> Entity:
     return Entity(model='cube',
-                  position=(x,width/2,z),
+                  position=(x, width / 2, z),
                   collider='box',
                   scale=(width, width, width),
                   texture='brick',
@@ -15,29 +15,30 @@ def createMyCube(x, z, width, mycolor) -> Entity:
                   )
 
 
-def createWall(x,z,width,height, mycolor=None, corner = 0) -> Entity:
+def createWall(x, z, width, height, mycolor=None, corner=0) -> Entity:
     return Entity(model='cube',
-                  position=(x,height/2,z),
+                  position=(x, height / 2, z),
                   collider='box',
                   scale=(width, height, 20),
                   texture='brick',
                   color=mycolor,
-                  rotation_y = corner)
+                  rotation_y=corner)
 
 
-def createTree(x=0, z=0) -> Entity :
+def createTree(x=0, z=0) -> Entity:
     return Entity(model='asset/static/tree/Tree.obj',
-           texture='asset/static/tree/tree2.jpeg',
-           position=(x, 1, z), colider='box',
-           color=color.rgb(1, 59, 14),
-           scale=(40, 40, 40),
-           )
+                  texture='asset/static/tree/tree2.jpeg',
+                  position=(x, 1, z), colider='box',
+                  color=color.rgb(1, 59, 14),
+                  scale=(40, 40, 40),
+                  )
 
-def createHouse(x=0,z=0, corner = 0) -> Entity:
+
+def createHouse(x=0, z=0, corner=0) -> Entity:
     return Entity(
-            model='asset/static/house/farmhouse.obj',
-            position = (x,1,z),
-            texture = 'asset/static/house/Farmhouse Texture.jpg',
-            scale = 10,
-            rotation_y=corner,
-        )
+        model='asset/static/house/farmhouse.obj',
+        position=(x, 1, z),
+        texture='asset/static/house/Farmhouse Texture.jpg',
+        scale=10,
+        rotation_y=corner,
+    )
--- main.py
+++ main.py
@@ -1,43 +1,56 @@
 from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+
 from Map import Map
 from CustomLib import *
 
-
-def input(key):
-    global map, step
-    if key == 'w':
-        map.creativeCube.z += step
-    if key == 's':
-        map.creativeCube.z -= step
-    if key == 'a':
-        map.creativeCube.x -= step
-    if key == 'd':
-        map.creativeCube.x += step
-    if key =='space':
-        print("cube:","{",f"'x':{map.creativeCube.x}, 'height':20, 'z':{map.creativeCube.z}, 'color':color.green","},")
-        print("wall:","{",f"'x':{map.creativeCube.x}, 'height':80, 'width':150, 'z':{map.creativeCube.z}, 'color':color.rgb(128, 49, 4), 'corner':0 ", "},")
-        print("tree:", "{",f"'x':{map.creativeCube.x}, 'z':{map.creativeCube.z}", "},")
-        print("house:", "{", f"'x':{map.creativeCube.x}, 'z':{map.creativeCube.z}, 'corner':0", "},")
-        print("building:",
-              "{",
-              f"'x':{map.creativeCube.x-60}, 'height':80*2, 'width':150*2, 'z':{map.creativeCube.z+200}, 'color':color.rgb(145, 117, 6), 'corner':0 ",
-              "},",
-              "{",
-              f"'x':{map.creativeCube.x+200},'height':80*2, 'width':150*3, 'z':{map.creativeCube.z-60}, 'color':color.rgb(145, 117, 6), 'corner':90 ",
-              "},",
-              "{",
-              f"'x':{map.creativeCube.x}, 'height':80*2, 'width':150*2+100, 'z':{map.creativeCube.z-260}, 'color':color.rgb(145, 117, 6), 'corner':0 ",
-              "},",
-              "{",
-              f"'x':{map.creativeCube.x-200}, 'height':80*2, 'width':150*2, 'z':{map.creativeCube.z+50}, 'color':color.rgb(145, 117, 6), 'corner':90 ",
-              "},",
-              )
-    
+# def input(key):
+#     global map, step
+#     if key == 'w':
+#         map.creativeCube.z += step
+#     if key == 's':
+#         map.creativeCube.z -= step
+#     if key == 'a':
+#         map.creativeCube.x -= step
+#     if key == 'd':
+#         map.creativeCube.x += step
+#     if key =='space':
+#         print("cube:","{",f"'x':{map.creativeCube.x}, 'height':20, 'z':{map.creativeCube.z}, 'color':color.green","},")
+#         print("wall:","{",f"'x':{map.creativeCube.x}, 'height':80, 'width':150, 'z':{map.creativeCube.z}, 'color':color.rgb(128, 49, 4), 'corner':0 ", "},")
+#         print("tree:", "{",f"'x':{map.creativeCube.x}, 'z':{map.creativeCube.z}", "},")
+#         print("house:", "{", f"'x':{map.creativeCube.x}, 'z':{map.creativeCube.z}, 'corner':0", "},")
+#         print("building:",
+#               "{",
+#               f"'x':{map.creativeCube.x-60}, 'height':80*2, 'width':150*2, 'z':{map.creativeCube.z+200}, 'color':color.rgb(145, 117, 6), 'corner':0 ",
+#               "},",
+#               "{",
+#               f"'x':{map.creativeCube.x+200},'height':80*2, 'width':150*3, 'z':{map.creativeCube.z-60}, 'color':color.rgb(145, 117, 6), 'corner':90 ",
+#               "},",
+#               "{",
+#               f"'x':{map.creativeCube.x}, 'height':80*2, 'width':150*2+100, 'z':{map.creativeCube.z-260}, 'color':color.rgb(145, 117, 6), 'corner':0 ",
+#               "},",
+#               "{",
+#               f"'x':{map.creativeCube.x-200}, 'height':80*2, 'width':150*2, 'z':{map.creativeCube.z+50}, 'color':color.rgb(145, 117, 6), 'corner':90 ",
+#               "},",
+#               )
 
 
 app = Ursina()
+Sky()
 map = Map()
 step = 20
-EditorCamera()
-camera.z = -25
+# cube = Entity(model='cube', position=(0,5,0), scale = 1)
+player = FirstPersonController(position=(0, 0, 0), speed=200)
+camera.y = 50
+# EditorCamera()
+gun = Entity(
+    model='asset/static/gun/G32SMGModel.fbx',
+    texture='asset/static/gun/gun_blue_violet_texture.png',
+    scale=.18,
+    position=(1, -6, 0),
+    parent=camera,
+
+)
+
+# gun.texture = 'asset/static/gun/G32SMGModel_LightMetal_BaseMap.png'
 app.run()
